301f2bc0e8cecf4c6ec6cbc8f0e5c4f35ebe1574
Justin Lok <32407493+justinlok@users.noreply.github.com>
2019-03-21 14:08:19 -0700

    Initial push

---
+++ Assets/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody rb;
+    Quaternion q;
+    bool jump;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        q = transform.rotation;
+        jump = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w")){
+            rb.AddForce(new Vector3 (-1f,0.0f,0.0f) , ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a")){
+            rb.AddForce(new Vector3 (0.0f,0.0f,-1f), ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("s")){
+            rb.AddForce(new Vector3 (1f,0.0f,0.0f), ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("d")){
+            rb.AddForce(new Vector3 (0.0f,0.0f,1f), ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKeyDown("space") && jump == true){
+            rb.AddForce(new Vector3 (0.0f, 10.0f,0.0f), ForceMode.Impulse);
+            jump = false;
+        }
+
+        if (Input.GetKeyDown("r")){
+            reset();
+        }
+
+        if (transform.position.y < -15f){
+            reset();
+        }
+    }
+
+    void reset(){
+        transform.position = new Vector3 (0.3416838f,3.114f,0.07361376f);
+        rb.velocity = new Vector3 (0f,0f,0f);
+        transform.rotation = q;
+        rb.angularVelocity =  new Vector3 (0f,0f,0f);
+        jump = true;
+    }
+}
